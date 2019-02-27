34959d1e9b62c9484bc98c3515acdf963dc99003
HUSTwanglei <hzauwl@163.com>
2019-02-27 23:04:07 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-this_directory = os.path.abspath(os_path.dirname(__file__))
+this_directory = os.path.abspath(os.path.dirname(__file__))
 def read_file(filename):
     with open(os.path.join(this_directory, filename), encoding='utf-8') as f:
         long_description = f.read()
