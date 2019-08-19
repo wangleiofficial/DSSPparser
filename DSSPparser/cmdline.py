9f659fa88f0e29de91856c05a98779bf32771936
neolei <hzauwl@163.com>
2019-08-19 21:02:33 +0800

    fix cmdline

--- DSSPparser/cmdline.py
+++ DSSPparser/cmdline.py
@@ -7,9 +7,11 @@ from DSSPparser import pdbToxssp, inputCollection, outputCollection
 
 def main():
     parser = argparse.ArgumentParser(description='cmd line pdbToxssp')
-    parser.add_argument('input', dest='input', type=str, help='input context')
-    parser.add_argument("--informat", type=str, dest='infortmat', help='input format', choices=inputCollection)
-    parser.add_argument("--outformat", type=str, dest='outformat', help='output format', choices=outputCollection)
+    parser.add_argument('input', type=str, help='input context')
+    parser.add_argument("--informat", type=str, dest='infortmat', help='input format', choices=inputCollection,
+                        default='pdb_id')
+    parser.add_argument("--outformat", type=str, dest='outformat', help='output format', choices=outputCollection,
+                        default='dssp')
     args = parser.parse_args()
     pdbToxssp(args.input, inputF=args.infortmat, outputF=args.outformat)
 
